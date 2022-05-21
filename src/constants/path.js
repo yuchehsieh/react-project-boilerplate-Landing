3e47453c4d805319bfe75981466d78d15936d10f
Allen Chen (Invoker) <e2ne00@gmail.com>
2022-05-21 09:00:37 +0800

    feat: add downloaded file for testing

---
+++ src/constants/path.js
@@ -0,0 +1,11 @@
+export default {
+    home: '/',
+    page1: '/page1',
+    page_with_params: '/params',
+
+    unauthorized: '/unauthorized',
+
+    sign_in: '/sign-in',
+    admin_dashbaord: '/admin-dashboard',
+    editor_product: '/editor-product',
+};
