58302da3642bec232f48f4b7e6f3bad40ff414cf
Allen Chen (Invoker) <e2ne00@gmail.com>
2022-05-22 13:09:36 +0800

    feat: include honme3 font

--- src/layouts/Home3/index.js
+++ src/layouts/Home3/index.js
@@ -8,9 +8,21 @@ import './style.css';
 import ComingSoon from './views/coming-soon';
 import Profile from './views/profile';
 import Home from './views/home';
+import { Helmet } from 'react-helmet';
 
 const Home3 = () => {
-    return <Outlet />
+    return (
+        <>
+            <Helmet>
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap"
+                    data-tag="font"
+                />
+            </Helmet>
+            <Outlet />
+        </>
+    );
 };
 
 export default Home3;
